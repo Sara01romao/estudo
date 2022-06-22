218583aeb8333b143ac8ed3dc9bfcde30e2d10b8
Sara Romão <sara-romao@live.com>
2022-06-21 23:55:25 -0300

    organização arquivos

---
+++ JavaScript /0-variaveis.js
@@ -0,0 +1,7 @@
+//let e const duas formas de declarar varáveis no ES6
+//let o valor pode ser alterado
+//const o valor é constante
+
+let idade = 10;
+const nome = "Sara";
+console.log("Nome:", nome, "idade: ", idade)
