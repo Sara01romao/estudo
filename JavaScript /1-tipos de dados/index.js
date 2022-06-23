6eed298e800ffdc6bdd496d5d0aad234cfc7c81d
Sara Romão <sara-romao@live.com>
2022-06-22 23:47:24 -0300

    operadores lógicos

--- JavaScript /1-tipos de dados/index.js
+++ JavaScript /1-tipos de dados/index.js
@@ -1,1 +1,22 @@
-//
+//Tipos de dados
+/*
+ * Number
+ * String
+ * Boolean
+ * Empty Values(null, undefined)
+ * 
+ */
+
+
+const idade = 21; // number
+const nome = 'Sara'; //string
+const feminino = true; //boolean
+
+// Boolenan Operadores Lógicos
+/**
+ * && -and
+ * ||-or
+ * ! - not
+ * 
+ */
+
