10bab9e48a6194bcf26b9bb4ef5d423989c5ebc0
Sara Romão <sara-romao@live.com>
2022-06-23 00:38:25 -0300

    exercicios tipos de dados

---
+++ JavaScript /1-tiposDeDados/exercicios.js
@@ -0,0 +1,22 @@
+//1- escreva três valores em string com aspas duplas, simples e template literals.
+const nome1 = "Lua";
+const nome2 = 'Sol';
+const nota = 10;
+
+console.log(nome1, nome2, `Nota: ${nota}`)
+
+
+
+//2- escreva três valores em number inteiros, decimal e aritmética.
+const idade = 10;
+const peso = 30.50;
+const altura = 10 - 8.5;
+console.log(`Idade: ${idade} Peso: ${peso} Altura: ${altura}`)
+
+//3- escreva três comparações com boolean maior, menor e igual
+const a =10;
+const b = 5;
+ console.log(10 > 5 ? true: false);
+ console.log(10 < 5 ? true: false);
+ console.log(10 === 5 ? true: false);
+
---
+++ JavaScript /1-tiposDeDados/index.js
@@ -0,0 +1,27 @@
+//Tipos de dados
+/*
+ * Number
+ * String
+ * Boolean
+ * Empty Values(null, undefined)
+ * 
+ */
+
+
+const idade = 2; // number
+const nome = 'Sara'; //string
+const feminino = true; //boolean
+
+// Boolenan Operadores Lógicos
+/**
+ * && -and
+ * ||-or
+ * ! - not
+ * 
+ */
+
+//Ternario faz compração em uma linha
+//condição ? se sim : se não
+console.log(idade > 17 ? "maior de idade" : "menor de idade")
+
+
