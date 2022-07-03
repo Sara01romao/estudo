ae9d86ddeb4698864a4dd2280c3a5952b47057d5
Sara Romão <sara-romao@live.com>
2022-07-02 23:15:27 -0300

    função Math

---
+++ JavaScript /2- EstruturasDeProgramacao/index.js
@@ -0,0 +1,14 @@
+//Maneiras de salvar valores
+
+var msg = "variável"
+const nome = "Sara"; // constante não altera o valor
+let idade = 26;
+
+
+//Função Nativa Math
+
+let maior = Math.max(2,5,36,4);
+console.log(maior);
+
+let arredondar = Math.round(5.55555);
+console.log(arredondar)
--- JavaScript /2- EstruturasDeProgramação/index.js
+++
@@ -1,5 +0,0 @@
-//Maneiras de salvar valores
-
-var msg = "variável"
-const nome = "Sara"; // constante não altera o valor
-let idade = 26;
