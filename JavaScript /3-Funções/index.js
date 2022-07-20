4f5e0739cea3bdb78676d51cfa958aa86e55a8b8
Sara Romão <sara-romao@live.com>
2022-07-19 23:01:59 -0300

    intro funções

--- JavaScript /3-Funções/index.js
+++ JavaScript /3-Funções/index.js
@@ -1,2 +1,22 @@
 //Funções são estruturas de código enores que são reaproveitadas durante a construção do programa
 //objetivo poupar repetição
+
+//exemplo estrutura
+function nomeFuncao(){
+    console.log('texto')
+}
+
+nomeFuncao()
+
+
+//função anônima
+const funcaoAnonima = function (){
+    console.log('Tesxto')
+}
+funcaoAnonima()
+
+//função com paramêtro
+const txtParametro = function(texto){
+    console.log(texto)
+}
+txtParametro('Textto parametro')
