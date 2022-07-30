c83eba6726cebc8952e846538b25875818ca99e4
Sara Romão <sara-romao@live.com>
2022-07-29 21:35:49 -0300

    arrow function

--- JavaScript /3-Funções/index.js
+++ JavaScript /3-Funções/index.js
@@ -20,3 +20,23 @@ const txtParametro = function(texto){
     console.log(texto)
 }
 txtParametro('Textto parametro')
+
+//ESCOPO- o que acontece dentro de uma função fica separado do escopo global 
+
+let n =20;
+
+const numero= function(){
+    let n = 5;
+    console.log(n);
+}
+
+console.log(n);
+
+//ARROW FUNCTIONS - uma forma de escrever funções
+const par = (n)=>{
+    return n % 2;
+}
+
+console.log(3)
+
+
