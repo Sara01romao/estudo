64a5beeb888455bfb2235a93851c84205658e666
Sara Romão <sara-romao@live.com>
2022-08-07 22:36:10 -0300

    exer funções

---
+++ JavaScript /3-Funções/exercicios.js
@@ -0,0 +1,29 @@
+//Escrever uma função que imprime "Hello World" no console.
+
+function hello(){
+    console.log("Hello World");
+}
+
+hello();
+
+//Escrever uma função que recebe um parâmetro de idade, que imprima "Você tem x anos" com template literals.
+const idade = 26;
+
+function msgAnos(idade){
+ console.log(`Você tem ${idade} anos`)
+}
+
+msgAnos(idade)
+
+
+// Escreva uma função que some dois números e retorne eles e imprima o retorno.
+
+function soma(){
+    const num1 = 3;
+    const num2 = 4;
+
+    return(num1+num2)
+}
+
+console.log(soma());
+
